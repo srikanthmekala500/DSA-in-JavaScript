3d21bf914f08f2df9a17fd4f83508257288e0545
srikanth mekala <125475567+srikanthmekala500@users.noreply.github.com>
2024-11-23 21:03:34 +0530

    Update ALL-in-one.js

--- ALL-in-one.js
+++ ALL-in-one.js
@@ -1,3 +1,16 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // Generate a random index between 0 and i (inclusive)
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        // Swap elements at i and randomIndex
+        [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+    }
+    return array;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+
 // accumulator
 // The value resulting from the previous call to callbackFn. On the first call, its value is initialValue if the latter is specified; otherwise its value is array[0].
 
