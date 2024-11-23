45d2601845ca58983aaa8f5f2eb3b0995109644a
srikanth mekala <125475567+srikanthmekala500@users.noreply.github.com>
2024-11-23 21:05:06 +0530

    Update ALL-in-one.js

--- ALL-in-one.js
+++ ALL-in-one.js
@@ -3,6 +3,9 @@ function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         // Generate a random index between 0 and i (inclusive)
         const randomIndex = Math.floor(Math.random() * (i + 1));
+        //let temp = array[i];
+        //array[i] = array[randomIndex];
+        //array[randomIndex] = temp;
         // Swap elements at i and randomIndex
         [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
     }
